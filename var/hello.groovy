b0f9175becbfe63f34ab2050511a941eed761e20
venkypv168-ship-it <venkypv168@gmail.com>
2025-09-30 21:00:28 +0530

    Create hello.groovy

---
+++ var/hello.groovy
@@ -0,0 +1,10 @@
+pipeline {
+    agent any
+    stages {
+        stage('welcome') {
+            steps {
+                script {
+                    hello('linux')
+                }
+            }
+        }
